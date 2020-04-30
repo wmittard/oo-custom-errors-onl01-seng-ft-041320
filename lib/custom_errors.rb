53cec85f1a4b9de07377bbd62d66d7526cdbf140
William Mittard <billymittard@ymail.com>
2020-04-30 00:13:56 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,8 +8,13 @@ class Person
   def get_married(person)
     self.partner = person
    if  person.class != Person 
+     begin
      raise 
      PartnerError
+     rescue 
+     PartnerError => error 
+     puts 
+     error.message
    else 
      person.partner = self 
    end 
